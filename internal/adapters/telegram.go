8fc26da5cf2a33b19e5fa921aef3d094f1a4f3b6
artem.ivanov <artem.ivanov@dif.tech>
2024-06-13 23:04:50 +0300

    uncomment code

--- internal/adapters/telegram.go
+++ internal/adapters/telegram.go
@@ -1,7 +1,10 @@
 package adapters
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"net/http"
 	"os"
 )
 
@@ -28,26 +31,25 @@ func NewNotifier() *Notifier {
 }
 
 func (n *Notifier) Send(text string) error {
-	fmt.Println("Sending message", text)
-	//msg := TelegramMessage{
-	//	ChatID: n.ChatID,
-	//	Text:   text,
-	//}
-	//
-	//body, err := json.Marshal(msg)
-	//if err != nil {
-	//	return err
-	//}
-	//
-	//resp, err := http.Post(n.URL, "application/json", bytes.NewBuffer(body))
-	//if err != nil {
-	//	return err
-	//}
-	//defer resp.Body.Close()
-	//
-	//if resp.StatusCode != http.StatusOK {
-	//	return fmt.Errorf("unexpected status: %s", resp.Status)
-	//}
+	msg := TelegramMessage{
+		ChatID: n.ChatID,
+		Text:   text,
+	}
+
+	body, err := json.Marshal(msg)
+	if err != nil {
+		return err
+	}
+
+	resp, err := http.Post(n.URL, "application/json", bytes.NewBuffer(body))
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("unexpected status: %s", resp.Status)
+	}
 
 	return nil
 }
